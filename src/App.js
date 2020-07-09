d8b5110c2847f1038bba813fa830a3312304c612
Sil Lavers <sillavers@Sils-MacBook-Pro.local>
2020-07-09 16:15:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sils Website Built from Sils Computer</h1>
+      <h2>THIS IS SO COOL</h2>
+      
     </div>
   );
 }
